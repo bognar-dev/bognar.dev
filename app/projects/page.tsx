7388bbb77231c935512c716c44d95bbc80d48d5c
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2023-10-24 17:39:47 +0100

    fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,7 +8,7 @@ export default async function Projects() {
     return (
       <main className="flex min-h-screen min-w-full flex-col  bg-background-100 px-5 md:px-20 w-full">
         <div>
-          {projectsData.map((project, index) => (
+          {projectsData.map((project:any, index:number) => (
             <Project key={index} project={project} />
           ))}
         </div>
