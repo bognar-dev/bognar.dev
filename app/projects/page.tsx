a5f77a9313e07b8765010caf4913b7c303951644
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2023-10-27 15:40:19 +0100

    data fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,13 +2,13 @@
 import Project from '@/components/project';
 
 export default async function Projects() {
-    const response = await fetch(`${process.env.BACKEND_URL}/api/projects`);
-    const projectsData = await response.json();
-
+    const response = await fetch(`${process.env.BACKEND_URL}/api/projects`)
+    if (response.ok) {
+      const data = await response.json();
     return (
       <main className="flex min-h-screen min-w-full flex-col  bg-background-100 px-5 md:px-20 w-full">
         <div>
-          {projectsData.map((project:any, index:number) => (
+          {data.map((project:any, index:number) => (
             <Project key={index} project={project} />
           ))}
         </div>
