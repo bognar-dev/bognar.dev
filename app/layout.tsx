113e11c43eb9c857e8a293a4e31c7464caab0b90
Niklas Bognar <niklas.bognar@gmail.com>
2023-11-11 00:19:34 +0100

    Moved Viewport to old next js version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import type { Metadata, Viewport } from 'next'
+
 import { Inter } from 'next/font/google'
-import {siteConfig } from '@/config/site';
+import { siteConfig } from '@/config/site';
 import '@/app/globals.css';
 import MainNav from '@/components/main-nav';
 
 import { ThemeProvider } from '@/components/theme-provider';
 import ThemeToggle from '@/components/theme-toggle';
 import { fontBody, fontHead } from '@/config/fonts';
+import { Metadata } from 'next';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,18 +16,16 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
-  
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
 }
-export const viewport:Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-}
+
 
 export default function RootLayout({
   children,
