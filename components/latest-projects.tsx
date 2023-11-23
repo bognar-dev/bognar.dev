d760e325b7d37635aae1e98e774bd954a79a35dd
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2023-11-23 15:40:10 +0000

    Add responsive layout for mobile devices in LatestProjects component

--- components/latest-projects.tsx
+++ components/latest-projects.tsx
@@ -5,13 +5,25 @@ import Card from './card';
 import { twMerge } from 'tailwind-merge';
 import SectionHeading from './section-header';
 import ProjectCard from './project-card';
+import { useMediaQuery } from 'react-responsive';
 
 export default async function LatestProjects({amount=2,className}:{amount?:number,className?:string}) {
+    const isMobile = useMediaQuery({ query: '(min-width: 700px)' });
     const response = await fetch(`${process.env.BACKEND_URL}/api/projects`, { next: { revalidate: 3600 } })
     const data  = await( response.json()) as Project[];
     data.sort((a,b)=>new Date(b.data.endDate).getTime() - new Date(a.data.endDate).getTime()).splice(amount)
 
-    
+    if(isMobile){
+        <>
+        <SectionHeading>Recent Projects:</SectionHeading>
+        <div className='grid lg:grid-cols-2 lg:grid-rows-1 gap-3  '>
+            {data.map((project: Project, key: number) => (
+                
+                <ProjectCard animate={false} project={project} key={key} className='bg-accent-50'/>
+            ))}
+        </div>
+        </>
+    }
     return (
         <Card className={twMerge('p-5',className)}>
         <SectionHeading>Recent Projects:</SectionHeading>
