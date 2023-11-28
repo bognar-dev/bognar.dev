cdba6a9b6c2f53a2aec501cd4dda72572aa74066
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2023-11-28 17:57:09 +0000

    Update card component styling

--- components/card.tsx
+++ components/card.tsx
@@ -4,13 +4,13 @@ import ScrollMotionDiv from "./scroll-motion-div"
 export default function Card({ className, children, motion = false }: { className?: string, children: React.ReactNode, motion?: boolean }) {
     if (motion) {
         return (
-            <ScrollMotionDiv className={twMerge(className, 'pt-3 w-full text-card-foreground grid bg-primary-50 backdrop-blur-sm shadow-sm shadow-primary-400 rounded-lg ')}>
+            <ScrollMotionDiv className={twMerge(className, 'pt-3 w-full text-card-foreground grid bg-background-50 backdrop-blur-sm shadow-sm shadow-primary-400 rounded-lg ')}>
                 {children}
             </ScrollMotionDiv>
         )
     }
     return (
-        <div className={twMerge(className, 'pt-3 w-full text-card-foreground grid bg-primary-50 backdrop-blur-sm shadow-sm shadow-primary-400 rounded-lg ')}>
+        <div className={twMerge(className, 'pt-3 w-full text-card-foreground grid bg-background-50 backdrop-blur-sm shadow-sm shadow-primary-400 rounded-lg ')}>
             {children}
         </div>
     )
