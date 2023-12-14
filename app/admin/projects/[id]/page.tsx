266292507ca369c3b8f3fc556a9711c6ce1cb910
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2023-12-14 16:42:36 +0000

    Refactor project page imports and variable names

--- app/admin/projects/[id]/page.tsx
+++ app/admin/projects/[id]/page.tsx
@@ -1,13 +1,11 @@
-import ProjectPreview from "@/components/project-preview";
-import ProjectView from "@/components/project-view";
 import { Project } from "@/types/project";
-import { ProjectEditForm, ProjectEditHeader, ProjectEditTag } from "../project-edit-form";
+import { ProjectEditForm, ProjectEditHeader, ProjectEditTag } from "@/app/admin/projects/project-edit-form";
 
 export async function generateStaticParams() {
   const response = await fetch(`${process.env.BACKEND_URL}/api/projects`, { next: { revalidate: 3600 } })
-  const posts = await (response.json()) as Project[];
-  return posts.map((post) => ({
-    id: String(post.id),
+  const projects = await (response.json()) as Project[];
+  return projects.map((project) => ({
+    id: String(project.id),
   }))
 }
 
@@ -15,13 +13,13 @@ export async function generateStaticParams() {
 
 export default async function ProjectPage({ params }: { params: { id: string } }) {
   const response = await fetch(`${process.env.BACKEND_URL}/api/projects/${params.id}`, { next: { revalidate: 3600 } })
-  const post = await (response.json()) as Project;
+  const project = await (response.json()) as Project;
   console.log(response.json())
   return (
     <>
-      <ProjectEditForm project={post}>
+      <ProjectEditForm project={project}>
         <ProjectEditHeader >
-          {post.data.tags.map((tag) => (
+          {project.data.tags.map((tag) => (
             <ProjectEditTag key={tag} tag={tag}>{tag}</ProjectEditTag>
           ))}
         </ProjectEditHeader>
