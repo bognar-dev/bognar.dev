e167f68b3749750147f9ea2e14d14f1db8b7c438
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2023-12-28 22:04:02 +0100

    Fonts added

--- components/title.tsx
+++ components/title.tsx
@@ -1,12 +1,12 @@
+import { brightMelody, beanCo ,think} from '@/config/fonts'
+
+export default function Title() {
 
 
-export default function Title(){
-   
-    
-  return(
-    <>
-      <h1 className=' text-5xl md:text-9xl font-plaster font-semibold'  >Bognar.dev</h1>
-    <div className='text-xl md:text-2xl pt-5 self-center text-center'>Where Algorithms Meet Aesthetics</div>
-    </>
+  return (
+    <div >
+      <h1 className={` text-9xl md:text-9xl font-brightMelody`}  >Bognar.dev</h1>
+      <div className='text-xl md:text-2xl pt-5 self-center text-center'>Where Algorithms Meet Aesthetics</div>
+    </div>
   )
-  }
+}
--- config/fonts.ts
+++ config/fonts.ts
@@ -1,15 +1,34 @@
 import { Inter, Arbutus } from 'next/font/google'
- 
+import localFont from 'next/font/local'
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-inter',
 })
- 
+
 const roboto_mono = Arbutus({
   weight: "400",
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-roboto-mono',
 })
- 
+
+export const beanCo = localFont({
+  src: './Beanco-Font.otf',
+  variable: '--font-bean-co',
+  display: 'swap',
+})
+
+export const brightMelody = localFont({
+  src: './Bright_Melody.otf',
+  variable: '--font-bright-melody',
+ display: 'swap',
+})
+
+
+export const think = localFont({
+  src: './Think.ttf',
+  variable: '--font-think',
+  display: 'swap',
+  weight: '800',
+})
