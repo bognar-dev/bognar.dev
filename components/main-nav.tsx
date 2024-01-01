f9532eb9059560ec86419e0787bf0b847d35b8f2
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2024-01-01 18:18:59 +0000

    Update font size in main navigation

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,7 +15,7 @@ export default function MainNav({ items }: MainNavProps) {
     <div className=" sticky top-0 z-40 w-full bg-white/10 backdrop-blur-sm rounded-b-sm">
       <div className="sticky top-0 z-50 flex flex-col gap-2 min-w-screen items-center justify-between divide-y-2 hover:divide-primary-300 divide-primary-100 pb-3 rounded-xl">
 
-        <Link href="/" className="font-head text-3xl flex items-center justify-items-center justify-center font-medium bg-opacity-10 hover:translate-x-1 hover:bg-secondary-200  ease-out duration-100 rounded-md px-4 mt-2">Bognar.dev</Link>
+        <Link href="/" className="font-head text-4xl flex items-center justify-items-center justify-center font-medium bg-opacity-10 hover:translate-x-1 hover:bg-secondary-200  ease-out duration-100 rounded-md px-4 mt-2">Bognar.dev</Link>
         {items?.length ? (
           <nav className="gap-2 flex md:gap-24 min-w-screen ">
             {items?.map(
@@ -25,7 +25,7 @@ export default function MainNav({ items }: MainNavProps) {
                     key={index}
                     href={item.href}
                     className=
-                    "text-2xl group flex items-center font-medium hover:translate-x-1 hover:bg-secondary-200 ease-out duration-100 rounded-md md:px-4 mt-1 py-1"
+                    "text-xl group flex items-center font-medium hover:translate-x-1 hover:bg-secondary-200 ease-out duration-100 rounded-md md:px-4 mt-1 py-1"
                   >
                     {item.title}
                     <Icons.arrowUpRight  className="group-hover:-translate-y-0.5  ease-out duration-100 w-2 h-2 md:w-4 md:h-4 ml-2"/>
