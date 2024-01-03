ffebc7066f5cd7d3fa300d782b4375d99c35c3b6
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2024-01-03 13:00:44 +0000

    Update about component text alignment

--- components/about.tsx
+++ components/about.tsx
@@ -14,7 +14,7 @@ const About = () => {
                 <Image className='rounded-xl w-20 h-20 md:w-40 md:h-40 self-center' src={profile} width={800} height={800} alt='profile-pic'></Image>
                 <div>
                 <SectionHeading>About me</SectionHeading>
-                    <p className='text-right text-lg'>{`Hey there! I'm Niklas, a down-to-earth person with roots in Germany.
+                    <p className='text-left leading-relaxed align-middle text-lg'>{`Hey there! I'm Niklas, a down-to-earth person with roots in Germany.
                     I've got a passion for cooking - not the fancy, over-the-top kind, but the practical, everyday recipes that make life a bit tastier.
 
     When I'm not in the kitchen, you'll find me sweating it out on the squash court, riding the waves while surfing, or enjoying a game of cricket. Sports are my way of staying active and having a good time.
