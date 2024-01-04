a645e1eee1bc886ffb54c1fc22259251fe652457
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2024-01-04 14:35:57 +0000

    Header animation

--- components/title.tsx
+++ components/title.tsx
@@ -1,14 +1,17 @@
+"use client"
+
 import fonts from "@/config/fonts"
 import { cn } from "@udecode/cn"
+import { motion } from "framer-motion"
+
+export default function Title() {
 
 
-export default function Title(){
-   
-    
-  return(
+  return (
     <>
-      <h1 className={cn(' text-5xl md:text-9xl  font-semibold',fonts.madeGentle.className)}  >Bognar.dev</h1>
-    <div className='text-xl md:text-2xl pt-5 self-center text-center'>Where Algorithms Meet Aesthetics</div>
+      <motion.h1 initial={{x:700}} animate={{ x: 0 }}
+  transition={{ type: "spring", stiffness: 100 , damping:15}} className={cn(' text-5xl md:text-9xl  font-semibold', fonts.madeGentle.className)}  >Bognar.dev</motion.h1>
+      <div className='text-xl md:text-2xl pt-5 self-center text-center'>Where Algorithms Meet Aesthetics</div>
     </>
   )
-  }
+}
