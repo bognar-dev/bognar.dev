00f7033754a036f59c18523efa511617ac06c645
niklas-laptop <niklas.bognar@gmail.com>
2024-07-25 11:39:31 +0100

    Fixed Theme toggle

--- app/(components)/theme-toggle.tsx
+++ app/(components)/theme-toggle.tsx
@@ -16,7 +16,7 @@ export default function ThemeToggle({className}:{className?:string}) {
 
     return (
         <motion.button
-            className={cn("group rounded-lghover:bg-primary-100 ease-in duration-100 z-10",className)}
+            className={cn("group rounded-lg hover:bg-primary-100 ease-in duration-100 z-10", className)}
             onClick={() => setTheme(theme === "light" ? "dark" : "light")}
             initial="initial"
             name="Toggle Theme"
@@ -27,8 +27,7 @@ export default function ThemeToggle({className}:{className?:string}) {
             variants={variants}
             transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
         >
-            <Icons.lightbulbToggle onclick={theme === "light"}/>
+            <Icons.lightbulbToggle theme={theme === "light"} />
         </motion.button>
     );
 }
-
