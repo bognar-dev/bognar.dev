7fb76f23633a31bcf639edc4f28290e7724709fa
bognar-dev <niklas.bognar@gmail.com>
2024-09-11 09:42:01 +0100

    Refactor footer component to update the height and positioning of the content

--- components/footer.tsx
+++ components/footer.tsx
@@ -60,11 +60,11 @@ const Nav = () => {
 export default function Footer() {
   return (
     <div 
-        className='relative h-[800px]'
+        className='relative h-[36rem]'
         style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}}
     >
-        <div className='relative h-[calc(100vh+800px)] -top-[100vh]'>
-            <div className='h-[800px] sticky top-[calc(100vh-800px)]'>
+        <div className='relative h-[calc(100vh+36rem)] -top-[100vh]'>
+            <div className='h-[36rem] sticky top-[calc(100vh-36rem)]'>
               <Content />
             </div>
         </div>
