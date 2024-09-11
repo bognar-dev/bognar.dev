9757babc7f5273ff0e15bab9ee46fea69d9fce08
bognar-dev <niklas.bognar@gmail.com>
2024-09-11 12:45:24 +0100

    Refactor contact page to add top padding to main container

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,7 @@ import Socials from '@/components/socials'
 export default function Home() {
   return (
     <PageWrapper>
-      <main className=" min-h-screen flex flex-col justify-center items-center gap-8">
+      <main className=" min-h-screen flex flex-col justify-center items-center gap-8 pt-20">
         <ContactForm/>
         <SignatureForm />
       </main>
