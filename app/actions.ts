b4c277d1d12540ee864c2b5f2f27e934f364e9c5
bognar-dev <niklas.bognar@gmail.com>
2024-09-11 15:14:20 +0100

    Refactor code to revalidate path after form submission and signature saving

--- app/actions.ts
+++ app/actions.ts
@@ -81,7 +81,7 @@ export async function submitForm(prevState: FormState, formData: FormData): Prom
 
   // Simulate a delay
   await new Promise(resolve => setTimeout(resolve, 1000))
-
+  revalidatePath('/')
   return { message: 'Form submitted successfully!' }
 }
 
@@ -101,7 +101,7 @@ export async function saveSignature(signatureData: string) {
     const filePath = path.join( 'public', 'signatures', fileName)
 
     const { url } = await put(filePath, buffer,{access: 'public'})
-
+    revalidatePath('/')
     return { success: true, message: 'Signature saved successfully!' }
   } catch (error) {
     console.error('Error saving signature:', error)
@@ -109,68 +109,7 @@ export async function saveSignature(signatureData: string) {
   }
 }
 
-export const signIn = async (prevState: any, formData: FormData) => {
 
-  const username = formData.get("username");
-  const password = formData.get("password");
-  const user = {
-    username,
-    password
-  }
-  const options: RequestInit = {
-    cache: 'no-store' as RequestCache,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(user),
-  };
-  const response = await fetch(`${process.env.BACKEND_URL}/login`, options)
-  const data = await response.json()
-  console.log(data)
-  cookies().delete('jwt')
-  cookies().set('jwt', data.token)
-
-  if (data.status === "success" || data.status === 'Successfully authenticated user' || data.status === 'new token generated' || (data.token !== undefined || null)) {
-    console.log("success")
-    redirect('/admin/dashboard')
-
-  } else {
-    console.log('login failed')
-    return { message: 'Login failed' }
-  }
-
-};
-
-
-
-
-export const sendEditedProject = async (prevState: any, formData: FormData) => {
-
-  if (formData === null) {
-    return { message: 'updateProject failed' }
-  }
-  const token = cookies().get('jwt')
-  if (token === undefined || token.value === '') {
-    return { message: "token not defined" }
-  }
-  const tags = formData.getAll('tag')
-  formData.delete('tag')
-  formData.set('tags', JSON.stringify(tags))
-  console.log(formData)
-  const response: Response = await fetch(`${process.env.BACKEND_URL}/private/updateProject`, {
-    cache: 'no-store' as RequestCache,
-    method: 'POST',
-    headers: {
-      'Authorization': token.value,
-    },
-    body: formData
-  })
-
-  revalidatePath('/')
-  return { message: 'success' }
-
-}
 
 
 
