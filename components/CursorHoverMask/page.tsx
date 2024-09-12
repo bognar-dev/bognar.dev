12f678bd6681b62848cc84cb9926181388f1c34c
bognar-dev <niklas.bognar@gmail.com>
2024-09-12 11:00:59 +0100

    Refactor CursorHoverMask component: Adjust size based on screen dimension and hover state

--- components/CursorHoverMask/page.tsx
+++ components/CursorHoverMask/page.tsx
@@ -3,15 +3,19 @@ import styles from './page.module.scss'
 import { ReactNode, useState } from 'react';  
 import { motion } from 'framer-motion';
 import useMousePosition from '@/hooks/useMousePosition';
+import useWindow from '@/hooks/useWindow';
 
 export default function CursorHoverMask() {
 
   const [isHovered, setIsHovered] = useState(false);
   const { x, y } = useMousePosition();
-  const size = isHovered ? 1000 : 50;
-
+  const { dimension} = useWindow();
+  const isSmallScreen = dimension.width <= 768;
+  const size = isHovered ? (isSmallScreen ? 500 : 1000) : (isSmallScreen ? 25 : 50);
+  
   return (
     <main className={styles.main}>
+     
       <motion.div 
         className={styles.mask}
         animate={{
@@ -20,6 +24,7 @@ export default function CursorHoverMask() {
         }}
         transition={{ type: "tween", ease: "backOut", duration:0.5}}
       >
+         
           <p onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}}>
           A <span>web developer</span> with a passion for crafting dynamic websites and animations—where creativity meets code. Pushing boundaries while learning, working, and always aiming for innovative solutions.
           </p>
@@ -28,6 +33,7 @@ export default function CursorHoverMask() {
       <div className={styles.body}>
       <p>I&apos;m a <span>multi-skilled</span> Computer Science student, combining embedded systems knowledge with front-end magic to deliver powerful, responsive, and engaging digital experiences.</p>
       </div>
+      
 
     </main>
   )
