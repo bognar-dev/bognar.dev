5dbda1f3b74e73d1696dd6ecb299b83f66116645
Niklas Bognar <niklas.bognar@gmail.com>
2024-09-16 14:52:16 +0100

    revalidate on getting sigantures

--- lib/getSignatures.ts
+++ lib/getSignatures.ts
@@ -1,4 +1,5 @@
 import { list } from '@vercel/blob';
+import { revalidatePath } from 'next/cache';
 
 async function allSignatures() {
   const { blobs } = await list({
@@ -9,6 +10,7 @@ async function allSignatures() {
 
 export async function getSignatures() {
   const signatures = await allSignatures();
+  revalidatePath('/')
   return signatures.map(blob => ({
     url: blob.url,
     name: blob.pathname.split('/').pop() || '',
